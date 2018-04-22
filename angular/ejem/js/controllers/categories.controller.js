bdfc9fdf71b9079061cc2471c5658e936d4eb40c
RayPoz <Jose.santam@gmail.com>
2018-04-22 19:25:05 +0200

    wewe
    we

--- angular/ejem/js/controllers/categories.controller.js
+++ angular/ejem/js/controllers/categories.controller.js
@@ -2,7 +2,6 @@
 	angular.module("MenuApp")
 	.controller('CategoriesController', CategoriesController);
 
-
 	CategoriesController.$inject = ['MenuDataService', 'items']; 
 	function CategoryController(MenuDataService, items){
 		var categories = this; 
