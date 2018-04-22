a30e1a19b23a4a1a35ce117e064d0e837e9a09c8
RayPoz <Jose.santam@gmail.com>
2018-04-22 18:07:01 +0200

    44
    44

--- angular/ejem/js/main/routes.js
+++ angular/ejem/js/main/routes.js
@@ -12,12 +12,12 @@
     $stateProvider
       .state('home', {
         url: '/home',
-        templateUrl: 'https://jmsanta.github.io/coursera/angular/ejem/views/home.html'
+        templateUrl: 'https://jmsanta.github.io/coursera/angular/ejem/home.html'
       })
 
       .state('categories', {
         url: '/categories',
-        templateUrl: 'https://jmsanta.github.io/coursera/angular/ejem/views/categoriesList.html',
+        templateUrl: 'https://jmsanta.github.io/coursera/angular/ejem/categoriesList.html',
         controller: 'CategoriesController as categories',
         resolve: {
           categories: ['MenuDataService', function (MenuDataService) {
@@ -28,7 +28,7 @@
 
       .state('items', {
         url: '/items/{categoryShortName}',
-        templateUrl: 'https://jmsanta.github.io/coursera/angular/ejem/views/itemsDetail.html',
+        templateUrl: 'https://jmsanta.github.io/coursera/angular/ejem/itemsDetail.html',
         controller: 'ItemsController as items',
         resolve: {
           items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
