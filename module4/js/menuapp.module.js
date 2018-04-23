2cb2ed6587697583f58422f769708f6cf3452244
jmsanta <jose.santam@gmail.com>
2018-04-23 13:59:39 +0200

    test
    test

--- module4/js/menuapp.module.js
+++ module4/js/menuapp.module.js
@@ -1,43 +1,4 @@
+(function(){
 	'use strict';
-
-
-RoutesConfig.$inject = ['$stateProvider','$urlRouterProvider'];
-angular.module("MenuApp").config(RoutesConfig);
-
-function RoutesConfig($stateProvider, $urlRouterProvider) {
-
-  // Redirect to HOME if no other URL matches
-  $urlRouterProvider.otherwise('https://jmsanta.github.io/coursera/module4/views/home');
-
-  // Set up UI states
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: ' https://jmsanta.github.io/coursera/module4/views/home.html'
-    })
-
-    .state('categories', {
-      url: '/categories',
-      templateUrl: 'https://jmsanta.github.io/coursera/module4/views/categoriesList.html',
-	  controller: 'MenuDataServiceController as MenuController',
-	  resolve : {
-		  items:['MenuDataService', function(){
-			  return MenuDataService.getAllCategories();
-		  }]
-	  }
-    })
-
-	// item detail
-
-	.state('categories.item', {
-      url: '/categories/{itemId}',
-      templateUrl: 'https://jmsanta.github.io/coursera/module4/views/itemDetail.html',
-	  controller: 'itemServiceController as itemController',
-	  resolve: {
-		  items : ['$stateParams','MenuDataService',
-		  function() {
-			  return MenuDataService.getAllCategories(itemId);
-		  }]
-	  }
-    });
-}
+	angular.module('MenuApp', ['data', 'ui.router']);
+})();
--- module4/js/routes.js
+++ module4/js/routes.js
@@ -1,4 +1,46 @@
+
 (function(){
-	'use strict';
-	angular.module('MenuApp', ['data', 'ui.router']);
+
+angular.module("MenuApp")
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to HOME if no other URL matches
+  $urlRouterProvider.otherwise('/home');
+
+  // Set up UI states
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: ' https://jmsanta.github.io/coursera/module4/views/home.html'
+    })
+
+    .state('categories', {
+      url: '/categories',
+      templateUrl: 'https://jmsanta.github.io/coursera/module4/views/categoriesList.html',
+	  controller: 'MenuDataServiceController as MenuController',
+	  resolve : {
+		  items:['MenuDataService', function(){
+			  return MenuDataService.getAllCategories();
+		  }]
+	  }
+    })
+
+	// item detail
+
+	.state('categories.item', {
+      url: '/categories/{itemId}',
+      templateUrl: 'https://jmsanta.github.io/coursera/module4/views/itemDetail.html',
+	  controller: 'itemServiceController as itemController',
+	  resolve: {
+		  items : ['$stateParams','MenuDataService',
+		  function() {
+			  return MenuDataService.getAllCategories(itemId);
+		  }]
+	  }
+    });
+}
 })();
