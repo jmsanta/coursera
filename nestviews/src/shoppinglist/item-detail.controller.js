dd75b9f4ef0c1e616fe18ea70efca093fca16b0a
RayPoz <jose.santam@gmail.com>
2018-04-23 22:31:30 +0200

    test
    test

--- nestviews/src/shoppinglist/item-detail.controller.js
+++ nestviews/src/shoppinglist/item-detail.controller.js
@@ -8,10 +8,11 @@ angular.module('MenuApp')
 ItemDetailController.$inject = ['$stateParams', 'items', '$filter'];
 function ItemDetailController($stateParams, items, $filter) {
   var itemDetail = this;
+  var category = $stateParams.itemId;
   
   var foundElement = $filter('filter')(items.data.menu_items, function(d) {
                     var cat = d.short_name.toLowerCase();
-                    if (cat.includes(itemId)) {
+                    if (cat.includes(category)) {
                         return cat;
                     }
                 });
