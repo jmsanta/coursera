912e97527c9f65fde9f6f780e5f105a07fe9e5d1
jmsanta <jose.santam@gmail.com>
2018-04-24 16:38:22 +0200

    r
    r

--- modtemp/src/main-itemlist.controller.js
+++ modtemp/src/main-itemlist.controller.js
@@ -5,14 +5,14 @@ angular.module('MenuApp')
 .controller('ItemListController', ItemListController);
 
 // Version with resolving to 1 item based on $stateParams in route config
-ItemDetailController.$inject = ['$stateParams', 'items'];
-function ItemDetailController($stateParams, items) {
+ItemListController.$inject = ['$stateParams', 'items'];
+function ItemListController($stateParams, items) {
   var itemList = this;
   //var category = $stateParams.itemId.toLowerCase();
 
  itemList.items = items.data.menu_items;
 
-  $state.go("itemList", {object: itemList.items});
+  //$state.go("itemList", {object: itemList.items});
 
 }
 
