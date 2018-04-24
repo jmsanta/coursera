1dea72ef1df6cec83f27ae918a1d2e6b74664b66
jmsanta <jose.santam@gmail.com>
2018-04-24 16:31:29 +0200

    test
    test

---
+++ modtemp/src/main-categorylist.controller.js
@@ -0,0 +1,17 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('MainCategoryListController', MainCategoryListController);
+
+
+MainCategoryListController.$inject = ['MenuDataService', 'items'];
+function MainCategoryListController(MenuDataService, items) {
+  var mainList = this;
+  // get the data of categories.
+  mainList.items = items.data;
+
+    $state.go("itemList", {object: mainList.items});
+}
+
+})();
---
+++ modtemp/src/routes.js
@@ -0,0 +1,48 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/templates/home.template.html'
+  })
+
+  // Premade list page
+  .state('mainList', {
+    url: '/main-list',
+    templateUrl: 'src/templates/main-categorylist.template.html',
+    controller: 'MainCategoryListController as mainList',
+    resolve: {
+      items: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+
+  // Item detail
+  .state('itemList', {
+    url: '/item-list/{itemId}',
+    templateUrl: 'src/templates/main-itemlist.template.html',
+    controller: 'ItemListController as itemList',
+    resolve: {
+          items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
+            return MenuDataService.getItemsForCategory($stateParams.itemId);
+          }]
+    }
+  });
+
+}
+
+})();
