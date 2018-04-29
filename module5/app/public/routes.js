030624503f439ad8fadb42ae960f282cac16ad50
RayPoz <jose.santam@gmail.com>
2018-04-29 22:45:28 +0200

    s
    ds

---
+++ module5/app/public/routes.js
@@ -0,0 +1,38 @@
+(function () {
+'use strict';
+
+angular.module('ValidationApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/home');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/home',
+    templateUrl: 'https://jmsanta.github.io/coursera/module5/templates/home.html'
+  })
+
+  // myInfo
+  .state('myInfo', {
+    url: '/myInfo', // must be HTTPS to be valid.
+    templateUrl: 'https://jmsanta.github.io/coursera/module5/templates/myInfo.html'
+  })
+
+  // Item detail
+  .state('signin', {
+    url: '/signin', // must be HTTPS to be valid.
+    templateUrl: 'https://jmsanta.github.io/coursera/module5/templates/signup.html',
+    controller: 'SignupController as ctrl'
+	 
+  });
+
+}
+
+})();
