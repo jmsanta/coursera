beeeae9fb0d9b704704ee8e9c5e0e79dc1947557
jmsanta <jose.santam@gmail.com>
2018-04-30 13:39:09 +0200

    rr
    rr

--- module5/app/validationRestaurant.module.js
+++ module5/app/validationRestaurant.module.js
@@ -4,7 +4,7 @@
 /**
  * Restaurant module that includes the public module as a dependency
  */
-angular.module('validationRestaurantApp', ['public'])
+angular.module('validationRestaurant', ['public'])
 .config(config);
 
 config.$inject = ['$urlRouterProvider'];
