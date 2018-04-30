fac65cb36d4a1d112eae64bcb6633bcc72221913
RayPoz <jose.santam@gmail.com>
2018-04-30 18:24:01 +0200

    yy
    yy

--- module5/app/common/signup.controller.js
+++ module5/app/common/signup.controller.js
@@ -1,55 +1,51 @@
 (function () {
 
 angular.module('common')
-.controller('RegistrationController', RegistrationController);
+.controller('SignupController', SignupController);
 
-RegistrationController.$inject = ['MenuService','$scope']
-function RegistrationController(MenuService, $scope) {
-  var reg = this;
+SignupController.$inject = ['MenuService','$scope']
+function SignupController(MenuService, $scope) {
+  var signctrl = this;
 
+  signctrl.submit = function (signctrl) {
 
-  reg.submit = function () {
-    console.log(reg);
-    console.log(MenuService);
-    console.log(reg.user.dish);
-
-    MenuService.dishExists(reg.user.dish)
+  signctrl.checkExistance = function (signctrl) {
+    MenuService.dishExists(signctrl.user.dish)
     .then(function (response) {
-      reg.noDishMessage = "";
-      MenuService.registered = true;
-      MenuService.user = reg.user;
-      MenuService.dish = response;
-      reg.completed = true;
+      signctrl.noDishMessage = "";
       console.log(response);
     })
     .catch(function (response) {
       console.log(response);
-      reg.noDishMessage = "No such menu number exists";
+      signctrl.noDishMessage = "this menu does not exists";
+    });
+  }
+  
+  MenuService.dishExists(signctrl.user.dish)
+    .then(function (response) {
+      signctrl.noDishMessage = "";
+      MenuService.registered = true;
+      MenuService.user = signctrl.user;
+      MenuService.dish = response;
+      signctrl.completed = true;
+      console.log(response);
+    })
+    .catch(function (response) {
+      console.log(response);
+      signctrl.noDishMessage = "No such menu number exists";
     });
   };
 
-  reg.checkExistance = function () {
-    MenuService.dishExists(reg.user.dish)
-    .then(function (response) {
-      reg.noDishMessage = "";
-      console.log(response);
-    })
-    .catch(function (response) {
-      console.log(response);
-      reg.noDishMessage = "No such menu number exists";
-    });
-  }
-
-  reg.registered = function () {
+  signctrl.registered = function () {
     return MenuService.registered;
   }
 
-  reg.getRegisteredUser = function ()
+  signctrl.getRegisteredUser = function ()
   {
     return MenuService.user;
   }
 
-  reg.getDish = function ()
+  signctrl.getDish = function ()
   {
     return MenuService.dish;
   }
