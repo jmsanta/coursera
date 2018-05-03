bd16a67063bc375a5e3899160ad4f43a4b73ac7e
RayPoz <jose.santam@gmail.com>
2018-05-03 20:56:59 +0200

    add right server
    add server

--- module5/app/common/common.module.js
+++ module5/app/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://davids-restaurant.herokuapp.com')
+.constant('ApiPath', 'https://aqueous-caverns-97231.herokuapp.com')
 .config(config);
 config.$inject = ['$httpProvider'];
 function config($httpProvider) {
